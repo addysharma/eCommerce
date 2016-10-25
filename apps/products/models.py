dee0db5d9fa857bcff2f662196cd26b9b9dfd5a1
Daniel Kouba <koubaphotog@gmail.com>
2016-10-24 21:56:14 -0700

    getting the products side of things wired up

--- apps/products/models.py
+++ apps/products/models.py
@@ -3,3 +3,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=45)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+	name = models.CharField(max_length = 45)
+	category = models.ForeignKey(Category)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class Quantity(models.Model):
+	amount = models.IntegerField()
+	product = models.ForeignKey(Product)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+# class Order(model.Model):
+# 	user = models.ForeignKey(User)
+# 	#limited choices
+# 		#Cancelled
+# 		#Order in Process
+# 		#Order Shipped
+# 	# status = models.()
+# 	product = models.ForeignKey(Product)
+# 	created_at = models.DateTimeField(auto_now_add=True)
+# 	updated_at = models.DateTimeField(auto_now=True)
